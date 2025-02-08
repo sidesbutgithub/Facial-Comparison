5aefa8f980633fd39339e7ffbe2a96d68196f0f2
Sidney Wai <80106909+sidesbutgithub@users.noreply.github.com>
2025-02-08 01:59:16 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -68,12 +68,14 @@ cv2.destroyAllWindows()
 """
 
 
+image = scrapeSite("https://en.wikipedia.org/wiki/Tyler,_the_Creator")
 
-facesDB = face_recognition.load_image_file(scrapeSite("https://en.wikipedia.org/wiki/Marisa_Tomei"))
+facesDB = cv2.imread(image)
+
 print("image scraped")
 knownFaceLocations = face_recognition.face_locations(facesDB)
 knownFaceEncodings = face_recognition.face_encodings(facesDB, knownFaceLocations)
-print("faces found")
+print(f"{len(knownFaceEncodings)} faces found")
 frameCount = 0
 
 cap = cv2.VideoCapture(0)
@@ -87,40 +89,58 @@ while True:
         
         faceLocations = face_recognition.face_locations(rgbFrame)
         faceEncodings = face_recognition.face_encodings(rgbFrame, faceLocations)
-
-        locations = []
-        distances = []
-        for face in faceEncodings:
+        location = "Unknown"
+        distance = 1
+        if faceEncodings:
+            face = faceEncodings[0]
             matches = face_recognition.compare_faces(knownFaceEncodings, face)
-            location = "Unknown"
-            distance = 0
             faceDistances = face_recognition.face_distance(knownFaceEncodings, face)
             best_match_index = np.argmin(faceDistances)
-            print(type(faceDistances[best_match_index]))
+            """
             if matches[best_match_index]:
                 location = knownFaceLocations[best_match_index]
                 distance = faceDistances[best_match_index]
+            """
+            location = knownFaceLocations[best_match_index]
+            distance = 1-faceDistances[best_match_index]
 
-            distances.append(distance)
-            locations.append(location)
         
     frameCount = (frameCount+1)%5
 
-    for (top, right, bottom, left), confidence in zip(faceLocations, distances):
-        
+    if faceLocations:
+
+        (top, right, bottom, left) = faceLocations[0]
+
+        w = right-left
+        h = bottom-top
+
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-        # Draw a label with a name below the face
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, str(confidence), (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        font = cv2.FONT_HERSHEY_TRIPLEX
+        cv2.putText(frame, f"{round(distance, 2)} %", (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        cv2.rectangle(frame, (0,0), (w, h), (0, 0, 255), 2)
+        if distance:
+            (topC, rightC, bottomC, leftC) = knownFaceLocations[best_match_index]
+            print((topC, rightC, bottomC, leftC))
+            
+            print([leftC,rightC, topC,bottomC])
+
+            crop = facesDB[topC:bottomC, leftC:rightC]
+            cv2.imshow("cropped face", crop)
+
+            crop = cv2.resize(crop, (w, h))
+            frame[0:h, 0:w] = crop
+        else:
+            cv2.putText(frame, "face not found", (2, int(h/2)), font, 0.5, (255, 255, 255), 1)
 
 
     cv2.imshow("frame", frame)
-    print("frame processed")
     keyPress = cv2.waitKey(1)
     
     if keyPress == 27:
         break
+
+cv2.waitKey(1000)
 
 cap.release()
 cv2.destroyAllWindows()
