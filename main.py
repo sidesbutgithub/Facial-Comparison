6b0c468da76ce909115f38f1fe73d98f2d1e2206
Sidney Wai <80106909+sidesbutgithub@users.noreply.github.com>
2025-02-10 10:46:40 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 import face_recognition
 import numpy as np
 
-
+#saves an image of the webpage provided
 def scrapeSite(url, imgName = "scrape.png"):
     driver = webdriver.Firefox()
     driver.get(url)
@@ -12,75 +12,21 @@ def scrapeSite(url, imgName = "scrape.png"):
     driver.quit()
     return imgName
 
-"""
-cap = cv2.VideoCapture(0)
-img = cv2.imread(scrapeSite("https://en.wikipedia.org/wiki/Marisa_Tomei"))
-
-grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-face_classifier = cv2.CascadeClassifier(
-    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
-)
 
 
-face = face_classifier.detectMultiScale(grayscale, 1.1, 5, minSize=(40,40))
-
-print(len(face))
-
-for (x, y, w, h) in face:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 4)
-
-import matplotlib.pyplot as plt
-
-img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-plt.figure(figsize=(20,10))
-plt.imshow(img_rgb)
-plt.axis('off')
-
-print("finished")
-
-
-
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("Failed to capture")
-        break
-
-    grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_classifier.detectMultiScale(grayscale, 1.1, 5, minSize=(40,40))
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 255), 4)
-
-    cv2.imshow("frame", frame)
-    keyPress = cv2.waitKey(1)
-    
-    if keyPress == 27:
-        break
-
-
-
-
-cap.release()
-cv2.destroyAllWindows()
-
-"""
-
-
+#replace url with site to search for faces
 image = scrapeSite("https://en.wikipedia.org/wiki/Tyler,_the_Creator")
+
+
 
 facesDB = cv2.imread(image)
 
-print("image scraped")
 knownFaceLocations = face_recognition.face_locations(facesDB)
 knownFaceEncodings = face_recognition.face_encodings(facesDB, knownFaceLocations)
-print(f"{len(knownFaceEncodings)} faces found")
+#print(f"{len(knownFaceEncodings)} faces found")
 frameCount = 0
 
 cap = cv2.VideoCapture(0)
-
-print("opening camera")
 
 while True:
     ret, frame = cap.read()
@@ -96,15 +42,16 @@ while True:
             matches = face_recognition.compare_faces(knownFaceEncodings, face)
             faceDistances = face_recognition.face_distance(knownFaceEncodings, face)
             best_match_index = np.argmin(faceDistances)
+            #use the if statement instead if you only want to provide likely face matches, currently provides best match even at low confidence
             """
             if matches[best_match_index]:
                 location = knownFaceLocations[best_match_index]
-                distance = faceDistances[best_match_index]
+                distance = (1-faceDistances[best_match_index])*100
             """
             location = knownFaceLocations[best_match_index]
-            distance = 1-faceDistances[best_match_index]
+            distance = (1-faceDistances[best_match_index])*100
 
-        
+    #process once every 5 frames, change mod to change process interval
     frameCount = (frameCount+1)%5
 
     if faceLocations:
@@ -117,30 +64,24 @@ while True:
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
         font = cv2.FONT_HERSHEY_TRIPLEX
+        #provide closeness of faces in box around user's face
         cv2.putText(frame, f"{round(distance, 2)} %", (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
         cv2.rectangle(frame, (0,0), (w, h), (0, 0, 255), 2)
         if distance:
+            #find closest face and display at top left of window
             (topC, rightC, bottomC, leftC) = knownFaceLocations[best_match_index]
-            print((topC, rightC, bottomC, leftC))
-            
-            print([leftC,rightC, topC,bottomC])
-
             crop = facesDB[topC:bottomC, leftC:rightC]
-            cv2.imshow("cropped face", crop)
-
             crop = cv2.resize(crop, (w, h))
             frame[0:h, 0:w] = crop
         else:
-            cv2.putText(frame, "face not found", (2, int(h/2)), font, 0.5, (255, 255, 255), 1)
+            cv2.putText(frame, "Face not found", (2, int(h/2)), font, 0.5, (255, 255, 255), 1)
 
 
     cv2.imshow("frame", frame)
     keyPress = cv2.waitKey(1)
-    
+    #go until esc is pressed
     if keyPress == 27:
         break
-
-cv2.waitKey(1000)
 
 cap.release()
 cv2.destroyAllWindows()
